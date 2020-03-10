707775c988bd24d88d23226a867da4afabb4815f
Tran Quoc Toan <tranquoctoan51199@gmail.com>
2020-03-10 11:56:31 +0700

    Updates

---
+++ precache-manifest.bb07744597d318388939ff3c49aaf52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8389ac5a620edb070b2c74ae8a3b9b3",
+    "url": "/FilmPedia/index.html"
+  },
+  {
+    "revision": "ca509db568fc7b75031b",
+    "url": "/FilmPedia/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "285086c9fa7fa3dba699",
+    "url": "/FilmPedia/static/js/2.214f8135.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/FilmPedia/static/js/2.214f8135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca509db568fc7b75031b",
+    "url": "/FilmPedia/static/js/main.3755227b.chunk.js"
+  },
+  {
+    "revision": "bae42b6cd262bd166457",
+    "url": "/FilmPedia/static/js/runtime-main.2e20aa01.js"
+  },
+  {
+    "revision": "2c9683e0a770a07fdaad3bc735daee23",
+    "url": "/FilmPedia/static/media/spinner.2c9683e0.gif"
+  }
+]);
